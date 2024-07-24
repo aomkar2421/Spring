c4718f801d262bcc350f753fa5e3f3557afa39a0
aomkar2421 <omkarjagtap1651@gmail.com>
2024-07-24 20:53:19 +0530

    24 July

--- 9_JDBC/src/main/java/om/main/App.java
+++ 9_JDBC/src/main/java/om/main/App.java
@@ -15,58 +15,72 @@ public class App
 	public static void main( String[] args )
 	{
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
-		
+
 		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
-		
-//		//Insert
-//		int roll = 005;
-//		String name = "XYZ";
-//		int marks = 91;
-//		
-//		String insert = "insert into student values(?,?,?)";
-//		int a = jdbcTemplate.update(insert, name, roll, marks);
-//		if (a>0) {
-//			System.out.println("Succesful");
-//		}else {
-//			System.out.println("error");
+
+		//		//Insert
+		//		int roll = 005;
+		//		String name = "XYZ";
+		//		int marks = 91;
+		//		
+		//		String insert = "insert into student values(?,?,?)";
+		//		int a = jdbcTemplate.update(insert, name, roll, marks);
+		//		if (a>0) {
+		//			System.out.println("Succesful");
+		//		}else {
+		//			System.out.println("error");
+		//		}
+
+
+		////		//Update
+		//		int roll = 002;
+		//		String name = "Omkar";
+		//		
+		//		String update = "update student set sname=? where sroll=?";
+		//		int a = jdbcTemplate.update(update, name, roll);
+		//		if (a>0) {
+		//			System.out.println("Succesful");
+		//		}else {
+		//			System.out.println("error");
+		//		}
+
+
+		////		//Delete
+		//		int roll = 103;
+		//		
+		//		String update = "delete from student where sname=?";
+		//		int a = jdbcTemplate.update(update, roll);
+		//		if (a>0) {
+		//			System.out.println("Succesful");
+		//		}else {
+		//			System.out.println("error");
+		//		}
+
+
+//		//Select
+//		String select = "select * from student";
+//		List<Student> stdList = jdbcTemplate.query(select, new StudentMapper());
+//
+//		for (Student std : stdList) {
+//			System.out.println("Name : "+std.getName());
+//			System.out.println("Marks : "+std.getMarks());
+//			System.out.println("Roll No : "+std.getRollno());
+//			System.out.println("=======================================");
 //		}
-		
-		
-////		//Update
-//		int roll = 002;
-//		String name = "Omkar";
-//		
-//		String update = "update student set sname=? where sroll=?";
-//		int a = jdbcTemplate.update(update, name, roll);
-//		if (a>0) {
-//			System.out.println("Succesful");
-//		}else {
-//			System.out.println("error");
-//		}
-		
-		
-////		//Delete
-//		int roll = 103;
-//		
-//		String update = "delete from student where sname=?";
-//		int a = jdbcTemplate.update(update, roll);
-//		if (a>0) {
-//			System.out.println("Succesful");
-//		}else {
-//			System.out.println("error");
-//		}
-		
-		
-		//Select
-		String select = "select * from student";
-		List<Student> stdList = jdbcTemplate.query(select, new StudentMapper());
+
+		//Select with where
+		String select = "select * from student where sroll=?";
+		int roll = 2;
+		List<Student> stdList = jdbcTemplate.query(select, new StudentMapper(),roll);
 
 		for (Student std : stdList) {
 			System.out.println("Name : "+std.getName());
-			System.out.println("Name : "+std.getMarks());
-			System.out.println("Name : "+std.getRollno());
+			System.out.println("Marks : "+std.getMarks());
+			System.out.println("Roll No : "+std.getRollno());
 			System.out.println("=======================================");
 		}
-		
+
+
+
 	}
 }
