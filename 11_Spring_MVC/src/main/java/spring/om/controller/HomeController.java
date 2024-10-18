59daf181248ad0d1c65fc119c75e63993422d879
aomkar2421 <omkarjagtap1651@gmail.com>
2024-10-18 22:19:49 +0530

    18 Oct

---
+++ 11_Spring_MVC/src/main/java/spring/om/controller/HomeController.java
@@ -0,0 +1,20 @@
+package spring.om.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	
+}
---
+++ 11_Spring_MVC/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! Spring mvc</h2>
+</body>
+</html>
